86ca0890d4112b64662047229088a6465d5416e1
unknown <bluakorvo@protonmail.com>
2025-03-28 08:43:46 -0600

    [AGREGA] un stack de navegacion para poder ver las demas pantallas

--- clon_fulanito/app/src/main/java/com/example/clon_fulanito/MainActivity.kt
+++ clon_fulanito/app/src/main/java/com/example/clon_fulanito/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
@@ -17,8 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.clon_fulanito.ui.pantallas.PantallaNavegadora
 import com.example.clon_fulanito.ui.theme.Clon_fulanitoTheme
 import com.example.clon_fulanito.vista_modelos.FulanitoViewModel
 
@@ -31,37 +35,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             Clon_fulanitoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PantallaDePublicaciones(vm_fulanito = modelo_app)
+                    //PantallaDePublicaciones(modifier = Modifier.padding(innerPadding), vm_fulanito = modelo_app)
+                    PantallaNavegadora(Modifier.padding(innerPadding), vm_fulanito = modelo_app)
                 }
             }
         }
     }
 }
 
-@Composable
-fun PantallaDePublicaciones(vm_fulanito: FulanitoViewModel){
-    val publicaciones_descargadas by vm_fulanito.publicaciones.observeAsState(emptyList())
 
-    LaunchedEffect(Unit) {
-        vm_fulanito.descargar_todas_las_publicaciones()
-    }
-
-    Column {
-        if(publicaciones_descargadas.isEmpty()){
-            Text("Aqui deberia colocar una barra de cargando")
-        }
-        else {
-            LazyColumn {
-                items(publicaciones_descargadas){ publicacion ->
-                    Text("Titulo: ${publicacion.title}")
-                    Text("${publicacion.body}")
-                    HorizontalDivider()
-                }
-
-            }
-        }
-    }
-}
 
 
 @Preview(showBackground = true)
