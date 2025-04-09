e7006be85dda78b6071b18aac508e8835c8262c2
unknown <bluakorvo@protonmail.com>
2025-04-09 08:42:53 -0600

    [AGREGA] una maquina de estados para SWAPIModelo y ahora detecta cuando agitan el celular

--- clon_fulanito/app/src/main/java/com/example/clon_fulanito/vista_modelos/SWAPIModelo.kt
+++ clon_fulanito/app/src/main/java/com/example/clon_fulanito/vista_modelos/SWAPIModelo.kt
@@ -10,21 +10,41 @@ import com.example.clon_fulanito.modelos.swapi.PaginaContenedora
 import kotlinx.coroutines.launch
 
 
+enum class Estados{
+    cargando,
+    mostrando_lista_de_naves,
+    mostrnado_nave,
+    error,
+    registrar_frustracion
+}
+
+
 class SWAPIModelo: ViewModel(){
     private val repositorio = RepositorioSWAPI()
 
     private val _pagina_actual = MutableLiveData<PaginaContenedora>()
     val pagina_actual: LiveData<PaginaContenedora> = _pagina_actual
 
+    private val _estado_actual = MutableLiveData<Estados>(Estados.cargando)
+    val estado_actual: LiveData<Estados> = _estado_actual // Poner la variable en una vitrina
+
+    private val _mensaje = MutableLiveData("Por ahora nada")
+    val mensaje: LiveData<String> = _mensaje
+
+
     fun descargar_pagina(pagina: Int = 1){
+        _estado_actual.postValue(Estados.cargando)
         Log.v("Pagina a descargar", "pagina: ${pagina}")
         viewModelScope.launch {
             try {
                 val pagina = repositorio.obtener_naves_espaciales(pagina)
                 _pagina_actual.postValue(pagina)
+                _estado_actual.postValue(Estados.mostrando_lista_de_naves)
             }
             catch (error: Exception){
                 Log.v("DESCARGA DE PAGINA SWAPI", "${error.message}")
+                _mensaje.postValue(error.message)
+                _estado_actual.postValue(Estados.error)
             }
         }
     }
@@ -45,5 +65,9 @@ class SWAPIModelo: ViewModel(){
         if(pagina_anterior != null){
             descargar_pagina(pagina_anterior)
         }
+    }
+
+    fun indicar_un_problema(){
+        _estado_actual.postValue(Estados.registrar_frustracion)
     }
 }
